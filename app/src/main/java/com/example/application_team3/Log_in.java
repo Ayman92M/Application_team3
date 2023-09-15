126166e47f312d620df64b2eb4fb083ad5c05810
ayman92m <ayman7mostafa@gmail.com>
2023-09-15 12:20:33 +0200

    master 1

--- app/src/main/java/com/example/application_team3/Log_in.java
+++ app/src/main/java/com/example/application_team3/Log_in.java
@@ -2,13 +2,27 @@ package com.example.application_team3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Log_in extends AppCompatActivity {
-
+    TextView signup_bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        signup_bt = findViewById(R.id.textView_signup);
+        signup_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent page = new Intent(Log_in.this, Sign_up2.class);
+                startActivity(page);
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/application_team3/MainActivity.java
+++ app/src/main/java/com/example/application_team3/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.application_team3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -17,5 +20,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_language);
 
         Database db = new Database();
+        Button _english = findViewById(R.id.button);
+
+        _english.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //setContentView(R.layout.activity_log_in);
+                Intent page1 = new Intent(MainActivity.this, Log_in.class);
+                startActivity(page1);
+            }
+        });
+
     }
 }
