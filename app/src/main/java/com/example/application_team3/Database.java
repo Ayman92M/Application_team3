65bae290c969ba63e4df95c41d98649b9bfc2f4a
Renseg <maxvg@live.se>
2023-09-17 19:07:59 +0200

    Changed the structure for the Entry classes. Fixed checkLogin* functions: return true if the given pid(personal ID) and pin/password matches an entry in the database. Else return false.

--- app/src/main/java/com/example/application_team3/Database.java
+++ app/src/main/java/com/example/application_team3/Database.java
@@ -1,18 +1,14 @@
 package com.example.application_team3;
 
-import android.util.Log;
-
 import androidx.annotation.NonNull;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Objects;
 
 public class Database {
 
@@ -21,45 +17,63 @@ public class Database {
     DatabaseReference elderlyRef;
     ElderlyEntry _elderly;
     CaregiverEntry _caregiver;
+
+    boolean loginSuccess;
     public Database() {
         rootNode = FirebaseDatabase.getInstance();
         caregiverRef = rootNode.getReference("Caregiver");
         elderlyRef = rootNode.getReference("Elderly");
 
-        checkLoginElderly("brimar100");
-
     }
 
-    public void registerCaregiver(String name, String pid, String password, String phoneNo, String dob, String country, String city){
-        _caregiver = new CaregiverEntry(name, pid, password, phoneNo, dob, country, city);
+    public void registerCaregiver(String name, String pid, String password, String phoneNo, String birthday, String address){
+        _caregiver = new CaregiverEntry(name, pid, password, phoneNo, birthday, address, null);
         caregiverRef.child(pid).setValue(_caregiver);
     }
 
-    public void registerElderly(String name, String pid, int pin, String phoneNo, String dob, String country, String city){
-        _elderly = new ElderlyEntry(name, pid, pin, phoneNo, dob, country, city);
+    public void registerElderly(String name, String pid, int pin, String phoneNo, String birthday, String address, String caregiver){
+        _elderly = new ElderlyEntry(name, pid, pin, phoneNo, birthday, address, caregiver);
         elderlyRef.child(pid).setValue(_elderly);
     }
 
-    public void checkLoginElderly(String pid){
+    public boolean checkLoginElderly(String pid, int pin){
+        loginSuccess=false;
         elderlyRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if(snapshot.child(pid).exists()){
-                    Log.d("PRINT", (String) snapshot.child(pid).child("name").getValue());
-                }
-                else {
-
+                    if(Objects.equals(snapshot.child(pid).child("pin").getValue(), pin)){
+                        loginSuccess = true;
+                        _elderly = snapshot.child(pid).getValue(ElderlyEntry.class);
+                    }
                 }
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
         });
+
+        return loginSuccess;
     }
 
-    public void checkLoginCaregiver(String pid){
-        System.out.println(caregiverRef.child(pid).getKey());
+    public boolean checkLoginCaregiver(String pid, String password){
+        loginSuccess=false;
+        caregiverRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.child(pid).exists()){
+                    if(Objects.equals(snapshot.child(pid).child("pin").getValue(), password)){
+                        loginSuccess = true;
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return loginSuccess;
     }
 }
