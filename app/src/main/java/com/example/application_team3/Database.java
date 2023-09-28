076bb50188904bd186d6719400ae8dca94b78f37
Renseg <maxvg@live.se>
2023-09-28 12:15:51 +0200

    New method for getting a List of the related Elderly for a given Caregiver. New method for getting a List of the related Caregivers for a given Elderly. Modified ElderlyEntry to require a String caregiver where the name of the Caregiver registering an Elderly will be input. New method for updating Elderly. New method for updating Caregiver. New Meal-Plan part of the database is implemented: new methods for fetching Meal-Plan database, fetching all Meal-Plans for a given Elderly pid, fetching a Meal-Plan for a given Elderly pid on a given date.

--- app/src/main/java/com/example/application_team3/Database.java
+++ app/src/main/java/com/example/application_team3/Database.java
@@ -9,6 +9,8 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Database {
@@ -16,13 +18,13 @@ public class Database {
     FirebaseDatabase rootNode;
     DatabaseReference caregiverRef;
     DatabaseReference elderlyRef;
-    ElderlyEntry _elderly;
-    CaregiverEntry _caregiver;
+    DatabaseReference mealPlanRef;
 
     public Database() {
         rootNode = FirebaseDatabase.getInstance();
         caregiverRef = rootNode.getReference("Caregiver");
         elderlyRef = rootNode.getReference("Elderly");
+        mealPlanRef = rootNode.getReference("Meal-Plan");
     }
 
     public Task<DataSnapshot> fetchCaregiverDB(){
@@ -31,7 +33,9 @@ public class Database {
     public Task<DataSnapshot> fetchElderlyDB(){
         return elderlyRef.get();
     }
-
+    public Task<DataSnapshot> fetchMealPlanDB(){
+        return mealPlanRef.get();
+    }
 
     public Task<DataSnapshot> fetchCaregiver(String pid){
         return caregiverRef.child(pid).get();
@@ -41,13 +45,53 @@ public class Database {
     }
 
     public void registerCaregiver(String name, String pid, String password, String phoneNo){
-        _caregiver = new CaregiverEntry(name, pid, password, phoneNo);
-        caregiverRef.child(pid).setValue(_caregiver);
+        CaregiverEntry caregiver = new CaregiverEntry(name, pid, password, phoneNo);
+        caregiverRef.child(pid).setValue(caregiver);
+    }
+    public void updateCaregiver(CaregiverEntry caregiver){
+        caregiverRef.child(caregiver.getPid()).setValue(caregiver);
     }
 
-    public void registerElderly(String name, String pid, String pin, String phoneNo){
-        _elderly = new ElderlyEntry(name, pid, pin, phoneNo);
-        elderlyRef.child(pid).setValue(_elderly);
+    public void registerElderly(String name, String pid, String pin, String phoneNo, String caregiver){
+        ElderlyEntry elderly = new ElderlyEntry(name, pid, pin, phoneNo, caregiver);
+        elderlyRef.child(pid).setValue(elderly);
+    }
+    public void updateElderly(ElderlyEntry elderly){
+        elderlyRef.child(elderly.getPid()).setValue(elderly);
+    }
+
+    public Task<DataSnapshot> fetchMealPlan(String pid){
+        TaskCompletionSource<DataSnapshot> mealPlanTaskSource = new TaskCompletionSource<>();
+        Task<DataSnapshot> mealPlanTask = mealPlanTaskSource.getTask();
+        Task<DataSnapshot> mealPlanDBTask = fetchMealPlanDB();
+
+        Tasks.whenAll(mealPlanDBTask).addOnCompleteListener(task ->{
+            DataSnapshot mealPlanDB = mealPlanDBTask.getResult();
+
+            if(mealPlanDB.child(pid).exists()){
+                mealPlanTaskSource.setResult(mealPlanDB.child(pid));
+            }
+            else { mealPlanTaskSource.setResult(null); }
+        });
+        return mealPlanTask;
+    }
+    public Task<DataSnapshot> fetchMealPlanDate(String pid, String date){
+        TaskCompletionSource<DataSnapshot> mealPlanTaskSource = new TaskCompletionSource<>();
+        Task<DataSnapshot> mealPlanTask = mealPlanTaskSource.getTask();
+        Task<DataSnapshot> mealPlanDBTask = fetchMealPlanDB();
+
+        Tasks.whenAll(mealPlanDBTask).addOnCompleteListener(task ->{
+            DataSnapshot mealPlanDB = mealPlanDBTask.getResult();
+
+            if(mealPlanDB.child(pid).exists()){
+                if(mealPlanDB.child(pid).child(date).exists()){
+                    mealPlanTaskSource.setResult(mealPlanDB.child(pid).child(date));
+                }
+                else{ mealPlanTaskSource.setResult(null); }
+            }
+            else { mealPlanTaskSource.setResult(null); }
+        });
+        return mealPlanTask;
     }
 
     public Task<Boolean> checkLoginElderly(String pid, String pin){
@@ -65,7 +109,6 @@ public class Database {
             }
             boolSource.setResult(exists);
         });
-
         return bool;
     }
 
@@ -84,11 +127,52 @@ public class Database {
             }
             boolSource.setResult(exists);
         });
-
         return bool;
-
     }
 
+    public Task<List<ElderlyEntry>> ElderlyList(String caregiverPID){
+        TaskCompletionSource<List<ElderlyEntry>> listTaskSource = new TaskCompletionSource<>();
+        Task<List<ElderlyEntry>> listTask = listTaskSource.getTask();
+        Task<DataSnapshot> elderlyDBTask = fetchElderlyDB();
+        Task<DataSnapshot> caregiverTask = fetchCaregiver(caregiverPID);
+
+        Tasks.whenAll(elderlyDBTask, caregiverTask).addOnCompleteListener(task -> {
+            CaregiverEntry caregiver = caregiverTask.getResult().getValue(CaregiverEntry.class);
+            DataSnapshot elderlyDB = elderlyDBTask.getResult();
+            List<ElderlyEntry> elderlyList = new ArrayList<>();
+            if(caregiver.getElderly() != null){
+                for(String e_pid : caregiver.getElderly().keySet()){
+                    if(elderlyDB.child(e_pid).exists()){
+                        elderlyList.add(elderlyDB.child(e_pid).getValue(ElderlyEntry.class));
+                    }
+                }
+                listTaskSource.setResult(elderlyList);
+            }
+        });
+        return listTask;
+    }
+
+    public Task<List<CaregiverEntry>> CaregiverList(String elderlyPID){
+        TaskCompletionSource<List<CaregiverEntry>> listTaskSource = new TaskCompletionSource<>();
+        Task<List<CaregiverEntry>> listTask = listTaskSource.getTask();
+        Task<DataSnapshot> caregiverDBTask = fetchCaregiverDB();
+        Task<DataSnapshot> elderlyTask = fetchElderly(elderlyPID);
+
+        Tasks.whenAll(caregiverDBTask, elderlyTask).addOnCompleteListener(task -> {
+            ElderlyEntry elderly = elderlyTask.getResult().getValue(ElderlyEntry.class);
+            DataSnapshot caregiverDB = caregiverDBTask.getResult();
+            List<CaregiverEntry> caregiverList = new ArrayList<>();
+            if(elderly.getCaregivers() != null){
+                for(String c_pid : elderly.getCaregivers().keySet()){
+                    if(caregiverDB.child(c_pid).exists()){
+                        caregiverList.add(caregiverDB.child(c_pid).getValue(CaregiverEntry.class));
+                    }
+                }
+            }
+            listTaskSource.setResult(caregiverList);
+        });
+        return listTask;
+    }
 
 
 }
