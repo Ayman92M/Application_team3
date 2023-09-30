04bbe18cd9c6b6f5cce4b860c3d40e8ef03c5cc9
Renseg <maxvg@live.se>
2023-09-30 22:19:48 +0200

    New method for adding elderly in a caregiver's list of elderly. [assignElderly]: adds an entry with a elderly pid and name. The same is done to the related elderly. New method for removing elderly in a caregiver's list of elderly. [removeElderly]: removes the elderly from a caregiver node. It also removes the caregiver from the related elderly node. When fetching ElderlyList or CaregiverList the list is updated in the Firebase database if the list contains a person that doesn't exist

--- app/src/main/java/com/example/application_team3/Database.java
+++ app/src/main/java/com/example/application_team3/Database.java
@@ -149,6 +149,10 @@ public class Database {
                     if(elderlyDB.child(e_pid).exists()){
                         elderlyList.add(elderlyDB.child(e_pid).getValue(ElderlyEntry.class));
                     }
+                    else{
+                        caregiver.removeElderly(e_pid);
+                        updateCaregiver(caregiver);
+                    }
                 }
                 listTaskSource.setResult(elderlyList);
             }
@@ -171,12 +175,31 @@ public class Database {
                     if(caregiverDB.child(c_pid).exists()){
                         caregiverList.add(caregiverDB.child(c_pid).getValue(CaregiverEntry.class));
                     }
+                    else {
+                        elderly.removeCaregiver(c_pid);
+                        updateElderly(elderly);
+                    }
                 }
+
             }
             listTaskSource.setResult(caregiverList);
         });
         return listTask;
     }
+    public void assignElderly(String caregiver_pid, String caregiver_name, String elderly_pid, String elderly_name){
+        Task<DataSnapshot> elderlyDBTask = fetchElderlyDB();
+        Tasks.whenAll(elderlyDBTask).addOnCompleteListener(task -> {
+            DataSnapshot elderlyDB = elderlyDBTask.getResult();
+            if(elderlyDB.child(elderly_pid).exists()){
+                caregiverRef.child(caregiver_pid).child("elderly").child(elderly_pid).setValue(elderly_name);
+                elderlyRef.child(elderly_pid).child("caregivers").child(caregiver_pid).setValue(caregiver_name);
+            }
+        });
 
+    }
+    public void removeElderly(String caregiver, String elderly){
+        caregiverRef.child(caregiver).child("elderly").child(elderly).removeValue();
+        elderlyRef.child(elderly).child("caregivers").child(caregiver).removeValue();
+    }
 
 }
