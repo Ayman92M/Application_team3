1080369a16d4b39e2ffbefd0ec549ba79899814a
Sara Skoglund <saraskoglund96@gmail.com>
2023-10-04 20:21:09 +0200

    cargiver_elderly_page (inte riktigt klar)

---
+++ app/src/main/java/com/example/application_team3/CargiverElderlyPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.application_team3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CargiverElderlyPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cargiver_elderly_page);
+    }
+}
