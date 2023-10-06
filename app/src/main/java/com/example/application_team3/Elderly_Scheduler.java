dc0c33c20956d1c8f74c023edc9738d805379df9
nathalieskar <nathalie.skar@gmail.com>
2023-10-06 09:35:54 +0200

    Backend-dayplanner: function to send current date chosen in calenderview to activity_elderly_scheduler, UI looks weird though. This will be fixed

--- app/src/main/java/com/example/application_team3/Elderly_Scheduler.java
+++ app/src/main/java/com/example/application_team3/Elderly_Scheduler.java
@@ -2,13 +2,26 @@ package com.example.application_team3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class Elderly_Scheduler extends AppCompatActivity {
+
+    TextView chosenDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elderly_scheduler);
+
+        chosenDate = findViewById(R.id.textView_date);
+        Intent incomingIntent = getIntent();
+        String date = incomingIntent.getStringExtra("date");
+        Log.d("incoming intent", "chosen date" + date);
+        chosenDate.setText(date);
+
+
     }
 }
