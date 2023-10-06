affb5ace512cd0d9f4f72d60ff53ea7475f474e8
Sara Skoglund <saraskoglund96@gmail.com>
2023-10-06 17:09:07 +0200

    Personal info

---
+++ app/src/main/java/com/example/application_team3/PersonalInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.application_team3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_info);
+    }
+}
